302b6fcbed2e7b5032a32c653320d0846f0ff3ef
Josh LeBlanc <joshcapitals07@gmail.com>
2025-01-30 17:11:34 +0100

    collision handling added

--- src/hash_table.c
+++ src/hash_table.c
@@ -4,6 +4,9 @@
 
 #include "hash_table.h"
 
+#define HT_PRIME_1 137
+#define HT_PRIME_2 139
+
 static ht_item* ht_new_item(const char* k, const char* v);
 ht_hash_table* ht_new();
 static void ht_del_item(ht_item* i);
@@ -50,4 +53,10 @@ static int ht_hash(const char* s, const int a, const int m) {
         hash = hash % m;
     }
     return (int)hash;
+}
+
+static int ht_get_hash(const char* s, const int num_buckets, const int attempt) {
+    const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
+    const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
+    return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
 }
