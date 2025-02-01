fa8fcc758a9c6354c6e9ba1022510bff0131e267
Josh LeBlanc <joshcapitals07@gmail.com>
2025-02-01 23:24:51 +0100

    added insert (update), search and delete methods on the table

--- src/hash_table.h
+++ src/hash_table.h
@@ -8,3 +8,11 @@ typedef struct {
     int count;
     ht_item** items;
 } ht_hash_table;
+
+static ht_item* ht_new_item(const char* k, const char* v);
+ht_hash_table* ht_new();
+static void ht_del_item(ht_item* i);
+void ht_del_hash_table(ht_hash_table* ht);
+void ht_insert(ht_hash_table* ht, const char* key, const char* value);
+char* ht_search(ht_hash_table* ht, const char* key);
+void ht_delete(ht_hash_table* h, const char* key);
